6b262c92d6cec356cc851fb47b8c13472a400bb7
Cristyan Valera <cristyan.valera.81@gmail.com>
2023-04-06 18:56:37 -0400

    add readonly class

--- app/Dto/LtvCalculation.php
+++ app/Dto/LtvCalculation.php
@@ -4,14 +4,12 @@ declare(strict_types=1);
 
 namespace App\Dto;
 
-class LtvCalculation
+readonly class LtvCalculation
 {
     public function __construct(
-        public readonly float $propertyValue,
-        public readonly float $depositAmount,
-        public readonly float $netLoan,
-        public readonly float $ltv,
-    )
-    {
-    }
+        public float $propertyValue,
+        public float $depositAmount,
+        public float $netLoan,
+        public float $ltv,
+    ) {}
 }
